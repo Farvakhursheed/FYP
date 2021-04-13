189bd469b505969cf55127d39868dbeea8e88ab5
Farvakhursheed <75477098+Farvakhursheed@users.noreply.github.com>
2021-04-13 22:01:25 +0500

    Update settings.py

--- fyp/settings.py
+++ fyp/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'fyp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'mealcatcher',
         'USER': 'root',
         'PASSWORD': '',
